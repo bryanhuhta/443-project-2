6fe51e11214c6447c3e775717bcd04655e63c79e
bryanhuhta <br.huhta@gmail.com>
2018-09-15 23:53:18 -0500

    Created log4net logger.

--- KnapsackProblem.Startup/Program.cs
+++ KnapsackProblem.Startup/Program.cs
@@ -2,6 +2,9 @@
 {
     public class Program
     {
+        private static readonly log4net.ILog Log =
+            log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         public static void Main(string[] args)
         {
         }
