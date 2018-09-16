cc50b6e904a7d19447d8f655e9e5b15c2d85bef3
bryanhuhta <br.huhta@gmail.com>
2018-09-15 23:47:30 -0500

    Removed unused directives and added public identifiers.

--- KnapsackProblem.Startup/Program.cs
+++ KnapsackProblem.Startup/Program.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KnapsackProblem.Startup
+﻿namespace KnapsackProblem.Startup
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
         }
     }
