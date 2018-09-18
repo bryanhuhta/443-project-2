55f384114911e668afc33603bc9f9fb75b58fc91
bryanhuhta <br.huhta@gmail.com>
2018-09-17 20:22:20 -0500

    Created IGene interface.

---
+++ KnapsackProblem.Startup/Core/Gene/IGene.cs
@@ -0,0 +1,8 @@
+﻿namespace KnapsackProblem.Startup.Core.Gene
+{
+    public interface IGene
+    {
+        IContent Content { get; }
+        bool IsActive { get; set; }
+    }
+}
---
+++ KnapsackProblem.Startup/Core/Gene/SimpleGene.cs
@@ -0,0 +1,32 @@
+﻿namespace KnapsackProblem.Startup.Core.Gene
+{
+    public class SimpleGene : IGene
+    {
+        #region Properties
+
+        /// <summary>
+        /// The content of this gene represents.
+        /// </summary>
+        public IContent Content { get; }
+
+        /// <summary>
+        /// Indicates whether the gene is active.
+        /// </summary>
+        public bool IsActive { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates an instance of SimpleGene.
+        /// </summary>
+        /// <param name="content">The content of the gene.</param>
+        public SimpleGene(IContent content)
+        {
+            Content = content;
+        }
+
+        #endregion
+    }
+}
