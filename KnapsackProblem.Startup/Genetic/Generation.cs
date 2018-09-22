d4d9f4e8f6e788ac9623c18958af5c34f305337b
bryanhuhta <br.huhta@gmail.com>
2018-09-22 01:45:05 -0500

    Created Generation class.

---
+++ KnapsackProblem.Startup/Genetic/Generation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace KnapsackProblem.Startup.Genetic
+{
+    public class Generation
+    {
+        #region Properties
+
+        public List<Chromosome> Chromosomes { get; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Creates an instance of <see cref="Generation"/> from a list of <see cref="Chromosome"/>.
+        /// </summary>
+        /// <param name="chromosomes">A list of <see cref="Chromosome"/>.</param>
+        public Generation(List<Chromosome> chromosomes)
+        {
+            Chromosomes = chromosomes;
+        }
+
+        #endregion
+    }
+}
