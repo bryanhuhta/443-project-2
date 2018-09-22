51dfd8fd959e19d16103270de1e0c522cc58d253
bryanhuhta <br.huhta@gmail.com>
2018-09-22 01:58:27 -0500

    Added documentation.

--- KnapsackProblem.Startup/Core/Repository.cs
+++ KnapsackProblem.Startup/Core/Repository.cs
@@ -3,17 +3,31 @@ using KnapsackProblem.Startup.Genetic;
 
 namespace KnapsackProblem.Startup.Core
 {
+    /// <summary>
+    /// This class interfaces with <see cref="Genetic.Knapsack"/> and <see cref="Gene"/> data found on disk.
+    /// </summary>
     public class Repository
     {
         #region Properties
 
+        /// <summary>
+        /// <see cref="Knapsack"/> data.
+        /// </summary>
         public Knapsack Knapsack { get; }
+
+        /// <summary>
+        /// <see cref="Gene"/> data.
+        /// </summary>
         public List<Gene> Genes { get; }
 
         #endregion
 
         #region Constructor
 
+        /// <summary>
+        /// Creates an instance of <see cref="Repository"/>.
+        /// </summary>
+        /// <param name="reader">The <see cref="IKnapsackReader"/> used to populate the <see cref="Repository"/>.</param>
         public Repository(IKnapsackReader reader)
         {
             Knapsack = reader.GetKnapsack();
