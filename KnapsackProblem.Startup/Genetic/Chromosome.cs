18b5f3b4156329bd9159418a38a6544dacd1622a
bryanhuhta <br.huhta@gmail.com>
2018-09-22 01:29:06 -0500

    Added Weight and Profit properties to Chromosome. Added ToString() override.

--- KnapsackProblem.Startup/Genetic/Chromosome.cs
+++ KnapsackProblem.Startup/Genetic/Chromosome.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace KnapsackProblem.Startup.Genetic
 {
@@ -7,6 +8,8 @@ namespace KnapsackProblem.Startup.Genetic
         #region Properties
 
         public List<Gene> Genes { get; }
+        public int Weight => Genes.Where(gene => gene.IsActive).Sum(gene => gene.Weight);
+        public int Profit => Genes.Where(gene => gene.IsActive).Sum(gene => gene.Profit);
 
         #endregion
 
@@ -15,6 +18,15 @@ namespace KnapsackProblem.Startup.Genetic
         public Chromosome(List<Gene> genes)
         {
             Genes = genes;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public override string ToString()
+        {
+            return $"[ Total weight: {Weight}, Total profit: {Profit} ]";
         }
 
         #endregion
