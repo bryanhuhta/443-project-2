0759b719175ff1f5077ee0843320ff78e68da669
bryanhuhta <br.huhta@gmail.com>
2018-09-22 02:12:23 -0500

    Added more documentation.

--- KnapsackProblem.Startup/Genetic/Chromosome.cs
+++ KnapsackProblem.Startup/Genetic/Chromosome.cs
@@ -3,18 +3,36 @@ using System.Linq;
 
 namespace KnapsackProblem.Startup.Genetic
 {
+    /// <summary>
+    /// This class encapsulates active and inactive <see cref="Gene"/> instances and the data associated with them.
+    /// </summary>
     public class Chromosome
     {
         #region Properties
 
+        /// <summary>
+        /// <see cref="Gene"/> instances for this <see cref="Chromosome"/>.
+        /// </summary>
         public List<Gene> Genes { get; }
+        
+        /// <summary>
+        /// The total weight of this <see cref="Chromosome"/>.
+        /// </summary>
         public int Weight => Genes.Where(gene => gene.IsActive).Sum(gene => gene.Weight);
+
+        /// <summary>
+        /// The total profit of this <see cref="Chromosome"/>.
+        /// </summary>
         public int Profit => Genes.Where(gene => gene.IsActive).Sum(gene => gene.Profit);
 
         #endregion
 
         #region Constructors
 
+        /// <summary>
+        /// Creates an instance of <see cref="Chromosome"/>.
+        /// </summary>
+        /// <param name="genes">A list of <see cref="Gene"/> instances.</param>
         public Chromosome(List<Gene> genes)
         {
             Genes = genes;
